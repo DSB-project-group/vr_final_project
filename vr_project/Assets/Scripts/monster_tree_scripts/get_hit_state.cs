9bd804b69c45083fc0309e5a4cfaf7ed8311e34f
d940909492 <dong5455993@gmail.com>
2023-12-01 14:04:50 -0500

    optimize monster animator status and update factory agent navi

--- vr_project/Assets/Scripts/monster_tree_scripts/get_hit_state.cs
+++ vr_project/Assets/Scripts/monster_tree_scripts/get_hit_state.cs
@@ -1,31 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class get_hit_state : StateMachineBehaviour
 {
     private int monster_current_health;
+    NavMeshAgent agent;
     //OnStateEnter is called before OnStateEnter is called on any state inside this state machine
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        monster_current_health = animator.GetInteger("monster_health");
-        animator.SetBool("isGetHit", false);
-        animator.SetInteger("monster_health", monster_current_health - 20);
+        agent = animator.GetComponent<NavMeshAgent>();
+        monster_current_health = animator.GetInteger("monsterHealth");
+        animator.SetInteger("monsterHealth", monster_current_health - 10);
+        Debug.Log("monster health" + monster_current_health);
     }
 
     //OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        if (monster_current_health <= 0)
+        {
+            animator.SetTrigger("isDeath");
+        }
     }
 
     //OnStateExit is called before OnStateExit is called on any state inside this state machine
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (monster_current_health <= 0)
-        {
-            animator.SetTrigger("isDeath");
-        }
+        agent.SetDestination(animator.transform.position);
+        animator.SetBool("isGetHit", false);
     }
 
     // OnStateMove is called before OnStateMove is called on any state inside this state machine
